291734fc30b6c2a736f9523c7469f2103f8c7e41
adorisse <djonnele.mamokosse@gmail.com>
2021-03-09 10:37:23 +0100

    ça marche

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const product = require('../controllers/Product.controller');
+const product = require('../controllers/product.controller');
 
 router.post('/products', product.create);
 router.get('/products/', product.getProducts);
